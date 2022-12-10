e4a3dc28dbd45556e553e9c9c437d8ee1ba1d2f0
rickyandrean99 <84983109+rickyandrean99@users.noreply.github.com>
2022-12-10 17:33:04 +0700

    Initial commit

---
+++ app/src/main/java/com/rickyandrean/jetcoffee/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.rickyandrean.jetcoffee
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.rickyandrean.jetcoffee.ui.components.SearchBar
+import com.rickyandrean.jetcoffee.ui.theme.JetCoffeeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetCoffeeTheme {
+                JetCoffeeApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun JetCoffeeApp() {
+    Column {
+        Banner()
+    }
+}
+
+@Composable
+fun Banner(
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp)
+        )
+        SearchBar()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetCoffeeTheme {
+        JetCoffeeApp()
+    }
+}
