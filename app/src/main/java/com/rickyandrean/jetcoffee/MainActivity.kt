f85a8d5eb69ea0255806e8acfef61f4f1793edf5
rickyandrean99 <84983109+rickyandrean99@users.noreply.github.com>
2022-12-10 20:17:15 +0700

    Category Section

--- app/src/main/java/com/rickyandrean/jetcoffee/MainActivity.kt
+++ app/src/main/java/com/rickyandrean/jetcoffee/MainActivity.kt
@@ -4,16 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.rickyandrean.jetcoffee.model.dummyCategory
+import com.rickyandrean.jetcoffee.ui.components.CategoryItem
 import com.rickyandrean.jetcoffee.ui.components.SearchBar
+import com.rickyandrean.jetcoffee.ui.components.SectionText
 import com.rickyandrean.jetcoffee.ui.theme.JetCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,6 +35,8 @@ class MainActivity : ComponentActivity() {
 fun JetCoffeeApp() {
     Column {
         Banner()
+        SectionText(stringResource(R.string.section_category))
+        CategoryRow()
     }
 }
 
@@ -49,6 +55,22 @@ fun Banner(
     }
 }
 
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(dummyCategory, key = { it.textCategory }) { category ->
+            CategoryItem(category)
+        }
+    }
+}
+
+// ==================== Preview Component ==================== //
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
